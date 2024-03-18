f7b957207146114512661dc7145aa1f1b14eb302
tcn741 <tianchenlko@126.com>
2024-03-18 17:43:01 +0800

    day-06

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
-<!-- <script>
-export default {
-  setup() {
-    console.log('setup')
-    const message = 'this is message'
-    const logMessage = () => {
-      console.log(message);
-    }
-    return {
-      message,
-      logMessage
-    }
-  },
-  beforeCreate () {
-    console.log('beforeCreate')
-  }
-}
-</script> -->
 <script setup>
-  const message = 'this is message';
-  const logMessage = () =>{
-    console.log(message)
-  }
-</script>
+  // 1. 导入函数
+  // import {reactive} from 'vue'
 
+  // // 2. 执行函数 传入一个对象类型的参数 变量接收
+  // const state = reactive({
+  //   count : 0
+  // })
+
+  // const setCount = () =>{
+  //   state.count++
+  // }
+  // 1.导入函数
+   import {ref} from 'vue'
+   // 2. 执行函数 传入参数[简单类型 + 对象类型] 变量接收
+   const count = ref(0)
+   console.log(count)
+   const setCount = () =>{
+     // 脚本区域修改ref产生的响应式对象数据 必须通过.value属性
+     count.value++
+   }
+  </script>
 <template>
-  <div>{{message}}
-    <button @click="logMessage">log</button>
+  <div>
+    <button @click="setCount">{{count}}</button>
   </div>
 </template>
